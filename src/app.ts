7b18d9e1a62429131bca3c64489156f8811925f7
choisangwon <rngus3698@naver.com>
2023-06-29 12:22:10 +0900

    first commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();	// express 객체 받아옴
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hi! This is my first express server');
+});	// HTTP GET method 정의
+
+app.listen('8000', () => {})	// 8000번 포트에서 서버 실행
