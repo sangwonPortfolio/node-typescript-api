87405ec0051780dae8f927772c365542e9a7c598
choisangwon <rngus3698@naver.com>
2023-06-29 12:44:53 +0900

    cors 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from "cors";
 
 const app = express();	// express 객체 받아옴
+
+const PORT = 8000;
+
+app.use(cors()); // CORS 이슈 해결
+app.use(express.urlencoded({ extended: true })); // query 받기
+app.use(express.json()); // body 받기
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hi! This is my first express server');
@@ -8,7 +15,14 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 app.use("/member", require("./routes/memberRouter"));
 
-app.listen('8000', () => {})	// 8000번 포트에서 서버 실행
+app.listen(PORT, () => {
+    console.log(`
+  ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  ┃   Server listening on port: ${PORT}    ┃
+  ┃     http://localhost:${PORT}/api       ┃
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+  `);
+})	// 8000번 포트에서 서버 실행
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     return res.status(500).json({ message: err.message });
